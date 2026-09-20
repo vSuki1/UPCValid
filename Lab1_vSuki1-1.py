f5c99a9420c751bec2427b4af45a7bc270886fff
Disukhi Ahmed <dahmed14@student.cscc.edu>
2026-09-20 19:02:55 -0400

    added user input

--- Lab1_vSuki1-1.py
+++ Lab1_vSuki1-1.py
@@ -7,7 +7,7 @@ Author: Disukhi Ahmed
 """
 
 
-def check_upc(first_eleven):
+def find_upc(first_eleven):
     sum_Of_Odd = first_eleven[0] + first_eleven[2] + first_eleven[4] + first_eleven[6] + first_eleven[8] + first_eleven[10]
     sum_Of_Odd = sum_Of_Odd * 3
     sum_Of_Even = first_eleven[1] + first_eleven[3] + first_eleven[5] + first_eleven[7] + first_eleven[9]
@@ -19,4 +19,10 @@ def check_upc(first_eleven):
             check_digit = 10 - m
     return check_digit
 
-    
+
+
+    UpcInput = input("Enter a 12 digit UPC code:")
+
+    checkDigitInput = int(UpcInput[11])
+
+    check_Digit_Calculated = find_upc(UpcInput)
