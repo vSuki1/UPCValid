586ced38a0311a5e413c6a69b16fc1eff5db4e3d
Disukhi Ahmed <dahmed14@student.cscc.edu>
2026-09-20 18:38:08 -0400

    Added sum of even of the array

--- Lab1_vSuki1-1.py
+++ Lab1_vSuki1-1.py
@@ -10,4 +10,4 @@ Author: Disukhi Ahmed
 def check_upc(first_eleven):
     sum_Of_Odd = first_eleven[0] + first_eleven[2] + first_eleven[4] + first_eleven[6] + first_eleven[8] + first_eleven[10]
     sum_Of_Odd = sum_Of_Odd * 3
-    
+    sum_Of_Even = first_eleven[1] + first_eleven[3] + first_eleven[5] + first_eleven[7] + first_eleven[9]
